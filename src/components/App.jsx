922a6e6da93cfe1374d4bea3c66fe07ce16fa844
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2022-07-23 20:10:00 +0200

    Koniec 6

---
+++ src/components/App.jsx
@@ -0,0 +1,80 @@
+import { useLocalStorage } from './hooks/useLocalStorage';
+import Section from './Section';
+import ContactList from './ContactList';
+import Phonebook from './Phonebook';
+import Filter from './Filter';
+import s from './App.module.css';
+import capitalize from 'utils/capitalize';
+
+const LOCAL_STORAGE_KEY_CONTACTS = 'contacts';
+const LOCAL_STORAGE_KEY_FILTER = 'filter';
+const INITIAL_STATE = {
+  contacts: [],
+  filter: '',
+};
+
+const App = () => {
+  const [contacts, setContacts] = useLocalStorage(
+    LOCAL_STORAGE_KEY_CONTACTS,
+    INITIAL_STATE.contacts
+  );
+  const [filter, setFilter] = useLocalStorage(
+    LOCAL_STORAGE_KEY_FILTER,
+    INITIAL_STATE.filter
+  );
+
+  const addContact = newContact => {
+    let doesAlreadyExist = false;
+    contacts.forEach(({ name }) => {
+      doesAlreadyExist = name === newContact.name;
+    });
+
+    if (doesAlreadyExist) {
+      alert(
+        `${capitalize(
+          newContact.name
+        )}is already in contacts.`
+      );
+      return;
+    }
+
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const handleFilterInput = event => {
+    setFilter(event.target.value);
+  };
+
+  const filterContacts = contacts => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const deleteContact = targetId => {
+    setContacts(contacts.filter(({ id }) => id !== targetId));
+  };
+
+  const handleDeleteContactBtnClick = event => {
+    deleteContact(event.target.dataset.id);
+  };
+
+  return (
+    <div className={s.container}>
+      <div className={s.app}>
+        <Section title="Phonebook">
+          <Phonebook addContact={addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter name={filter} inputHandler={handleFilterInput} />
+          <ContactList
+            contacts={filterContacts(contacts)}
+            deleteBtnHandler={handleDeleteContactBtnClick}
+          />
+        </Section>
+      </div>
+    </div>
+  );
+};
+
+export default App;
